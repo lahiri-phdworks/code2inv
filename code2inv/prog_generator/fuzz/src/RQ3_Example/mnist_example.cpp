e136a3e9238d41af81c7f32f7b0596d44a6b74e9
Sumit Lahiri <lahiri.phdworks@gmail.com>
2021-05-15 23:16:29 +0530

    WIP : RQ3 MNIST Example

--- code2inv/prog_generator/fuzz/src/RQ3_Example/mnist_example.cpp
+++ code2inv/prog_generator/fuzz/src/RQ3_Example/mnist_example.cpp
@@ -1,5 +1,9 @@
-#include <torch/torch.h>
 
+#include <torch/torch.h>
+#include <torch/script.h> // One-stop header.
+// https://github.com/pytorch/examples/blob/master/cpp/mnist/mnist.cpp
+
+#include <memory>
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
@@ -16,113 +20,30 @@ const int64_t kTrainBatchSize = 64;
 const int64_t kTestBatchSize = 1000;
 
 // The number of epochs to train.
-const int64_t kNumberOfEpochs = 10;
+const int64_t kNumberOfEpochs = 1;
 
 // After how many batches to log a new update with the loss value.
 const int64_t kLogInterval = 10;
 
-struct Net : torch::nn::Module
+int main(int argc, const char *argv[])
 {
-    Net()
-        : conv1(torch::nn::Conv2dOptions(1, 10, /*kernel_size=*/5)),
-          conv2(torch::nn::Conv2dOptions(10, 20, /*kernel_size=*/5)),
-          fc1(320, 50),
-          fc2(50, 10)
+    if (argc != 2)
     {
-        register_module("conv1", conv1);
-        register_module("conv2", conv2);
-        register_module("conv2_drop", conv2_drop);
-        register_module("fc1", fc1);
-        register_module("fc2", fc2);
+        std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+        return -1;
     }
 
-    torch::Tensor forward(torch::Tensor x)
+    torch::jit::script::Module module;
+    try
     {
-        x = torch::relu(torch::max_pool2d(conv1->forward(x), 2));
-        x = torch::relu(
-            torch::max_pool2d(conv2_drop->forward(conv2->forward(x)), 2));
-        x = x.view({-1, 320});
-        x = torch::relu(fc1->forward(x));
-        x = torch::dropout(x, /*p=*/0.5, /*training=*/is_training());
-        x = fc2->forward(x);
-        return torch::log_softmax(x, /*dim=*/1);
+        // Deserialize the ScriptModule from a file using torch::jit::load().
+        module = torch::jit::load(argv[1]);
     }
-
-    torch::nn::Conv2d conv1;
-    torch::nn::Conv2d conv2;
-    torch::nn::Dropout2d conv2_drop;
-    torch::nn::Linear fc1;
-    torch::nn::Linear fc2;
-};
-
-template <typename DataLoader>
-void train(
-    size_t epoch,
-    Net &model,
-    torch::Device device,
-    DataLoader &data_loader,
-    torch::optim::Optimizer &optimizer,
-    size_t dataset_size)
-{
-    model.train();
-    size_t batch_idx = 0;
-    for (auto &batch : data_loader)
+    catch (const c10::Error &e)
     {
-        auto data = batch.data.to(device), targets = batch.target.to(device);
-        optimizer.zero_grad();
-        auto output = model.forward(data);
-        auto loss = torch::nll_loss(output, targets);
-        AT_ASSERT(!std::isnan(loss.template item<float>()));
-        loss.backward();
-        optimizer.step();
-
-        if (batch_idx++ % kLogInterval == 0)
-        {
-            std::printf(
-                "\rTrain Epoch: %ld [%5ld/%5ld] Loss: %.4f",
-                epoch,
-                batch_idx * batch.data.size(0),
-                dataset_size,
-                loss.template item<float>());
-        }
+        std::cerr << "error loading the model\n";
+        return -1;
     }
-}
-
-template <typename DataLoader>
-void test(
-    Net &model,
-    torch::Device device,
-    DataLoader &data_loader,
-    size_t dataset_size)
-{
-    torch::NoGradGuard no_grad;
-    model.eval();
-    double test_loss = 0;
-    int32_t correct = 0;
-    for (const auto &batch : data_loader)
-    {
-        auto data = batch.data.to(device), targets = batch.target.to(device);
-        auto output = model.forward(data);
-        test_loss += torch::nll_loss(
-                         output,
-                         targets,
-                         /*weight=*/{},
-                         torch::Reduction::Sum)
-                         .template item<float>();
-        auto pred = output.argmax(1);
-        correct += pred.eq(targets).sum().template item<int64_t>();
-    }
-
-    test_loss /= dataset_size;
-    std::printf(
-        "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
-        test_loss,
-        static_cast<double>(correct) / dataset_size);
-}
-
-auto main() -> int
-{
-    torch::manual_seed(1);
 
     torch::DeviceType device_type;
     if (torch::cuda::is_available())
@@ -137,8 +58,15 @@ auto main() -> int
     }
     torch::Device device(device_type);
 
-    Net model;
-    model.to(device);
+    auto test_dataset = torch::data::datasets::MNIST(
+                            kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
+                            .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+                            .map(torch::data::transforms::Stack<>());
+
+    const size_t test_dataset_size = test_dataset.size().value();
+
+    auto test_loader =
+        torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
 
     auto train_dataset = torch::data::datasets::MNIST(kDataRoot)
                              .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
@@ -148,20 +76,40 @@ auto main() -> int
         torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
             std::move(train_dataset), kTrainBatchSize);
 
-    auto test_dataset = torch::data::datasets::MNIST(
-                            kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
-                            .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-                            .map(torch::data::transforms::Stack<>());
-    const size_t test_dataset_size = test_dataset.size().value();
-    auto test_loader =
-        torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
+    torch::NoGradGuard no_grad;
+    module.eval();
 
-    torch::optim::SGD optimizer(
-        model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5));
+    double test_loss = 0;
+    int32_t correct = 0;
 
-    for (size_t epoch = 1; epoch <= kNumberOfEpochs; ++epoch)
+    for (const auto &batch : *train_loader)
     {
-        train(epoch, model, device, *train_loader, optimizer, train_dataset_size);
-        test(model, device, *test_loader, test_dataset_size);
+        auto data = batch.data.to(device);
+        auto targets = batch.target.to(device);
+
+        std::vector<torch::jit::IValue> mnist_input;
+        std::vector<torch::jit::IValue> mnist_labelled_output;
+
+        mnist_input.push_back(data);
+        mnist_labelled_output.push_back(targets);
+
+        auto output = module.forward(mnist_input).toTensor();
+
+        test_loss += torch::nll_loss(
+                         output,
+                         targets,
+                         /*weight=*/{},
+                         torch::Reduction::Sum)
+                         .template item<float>();
+
+        auto pred = output.argmax(1);
+        std::cout << pred << std::endl;
+        correct += pred.eq(targets).sum().template item<int64_t>();
     }
+
+    test_loss /= test_dataset_size;
+    std::printf(
+        "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
+        test_loss,
+        static_cast<double>(correct) / test_dataset_size);
 }
--- code2inv/prog_generator/fuzz/src/RQ3_Example/mnist_example.py
+++ code2inv/prog_generator/fuzz/src/RQ3_Example/mnist_example.py
@@ -8,31 +8,39 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
-traced_script_module = None
-
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(1, 10, 5, 1)
+        self.conv2 = nn.Conv2d(10, 20, 5, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
+
+        #  x = torch::relu(torch::max_pool2d(conv1->forward(x), 2));
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
         x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+
+        # x = torch::relu(torch::max_pool2d(conv2_drop->forward(conv2->forward(x)), 2));
+        x = self.conv2(x)
         x = self.dropout1(x)
-        x = torch.flatten(x, 1)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+
+        # x = x.view({-1, 320});
+        x = x.view(-1, 320)
+
         x = self.fc1(x)
         x = F.relu(x)
+
         x = self.dropout2(x)
         x = self.fc2(x)
+
         output = F.log_softmax(x, dim=1)
         return output
 
@@ -167,7 +175,7 @@ def main():
         scheduler.step()
 
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        # torch.save(model.state_dict(), "mnist_cnn.pt")
         sm.save("mnist_model_cpp.pt")
 
 
